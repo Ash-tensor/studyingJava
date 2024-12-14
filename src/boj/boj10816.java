324da983292cdcefe3e49f0c3b42999566c241cf
ash <Tensor17s@gmail.com>
2024-12-14 14:48:15 +0900

    백준 문제 풀이

--- src/boj/boj10816.java
+++ src/boj/boj10816.java
@@ -1,101 +1,80 @@
 package boj;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class boj10816 {
+    static int[] intCards;
+    static int[] intTargetCards;
+    static List<Integer> sortedCards;
+    static List<Integer> sortedTargetCards;
+    static int cardNumber;
+    static int targetCardNumber;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<int[]> question = boj10816.inputMethod(scanner);
-        int[] cards = question.get(0);
-        int[] targetCards = question.get(1);
-
-        ArrayList<Integer> sortedCards = new ArrayList<>();
-        for (int x : cards) {
-            sortedCards.add(x);
-        }
-        sortedCards = quickSort(sortedCards);
-
-
-
-
-
-
+        inputMethod(scanner);
+        solution();
 
     }
-//    public static Object binarySearch(ArrayList<Integer> targetArray, int targetNumber) {
-//        // targetArray는 정렬되어 있다고 가정
-//
-//        int middle = targetArray.size() / 2;
-//        int point = targetArray.get(middle);
-//
-//        if (targetNumber < point) {
-//            List<Integer> complexSpace = targetArray.subList(0, middle);
-//            binarySearch(complexSpace)
-//        }
-//
-//
-//    }
-
-
-    public static ArrayList<Integer> quickSort(ArrayList<Integer> cards) {
-        if (cards.size() <= 1) {
-            return cards;
+    public static void solution() {
+        HashMap<Integer, Integer> cardMap = new HashMap<>();
+        for (int x : intTargetCards) {
+            cardMap.put(x, 0);
         }
 
-        // 정렬되면 끝
-        ArrayList<Integer> smaller = new ArrayList<>();
-        ArrayList<Integer> bigger = new ArrayList<>();
-        ArrayList<Integer> equal = new ArrayList<>();
+        for (int x : intCards) {
+            int nowNumber = cardMap.getOrDefault(x, 0);
+            cardMap.put(x, nowNumber + 1);
+        }
 
-        int point = cards.get(0);
+        StringBuilder answer = new StringBuilder();
 
-        for (int x : cards) {
-            if (x == point) {
-                equal.add(x);
+        for (int i = 0; i < intTargetCards.length; i++) {
+            int targetNumber = cardMap.get(intTargetCards[i]);
+            if (i == intTargetCards.length - 1) {
+                answer.append(targetNumber);
+
             }
             else {
-                // x가 기준점보다 작으면
-                if (x < point) {
-                    smaller.add(x);
-                }
-                else {
-                    bigger.add(x);
-                }
+                answer.append(targetNumber);
+                answer.append(" ");
             }
+
         }
 
-        ArrayList<Integer> answer = new ArrayList<>(quickSort(smaller));
-        answer.addAll(equal);
-        answer.addAll(quickSort(bigger));
+        System.out.print(answer);
+    }
 
-        return answer;
+    public static void inputMethod(Scanner scanner) {
+        cardNumber = Integer.parseInt(scanner.nextLine());
+        String cardString = scanner.nextLine();
+        String[] cards = cardString.split(" ");
+
+        intCards = Arrays.stream(cards).mapToInt(Integer::parseInt).toArray();
+
+        targetCardNumber = Integer.parseInt(scanner.nextLine());
+
+        String targetCardString = scanner.nextLine();
+        String[] targetCards = targetCardString.split(" ");
+
+        // 이건 순서대로 정렬되어 있을 것 같은데
+        intTargetCards = Arrays.stream(targetCards).mapToInt(Integer::parseInt).toArray();
+
+        sortedCards = new ArrayList<>();
+        sortedTargetCards = new ArrayList<>();
+
+        for (int x : intCards) {
+            sortedCards.add(x);
+        }
+        for (int x : intTargetCards) {
+            sortedTargetCards.add(x);
+        }
+
+
+        sortedCards.sort(Comparator.naturalOrder());
+        sortedTargetCards.sort(Comparator.naturalOrder());
     }
 
 
-    public static ArrayList<int[]> inputMethod(Scanner scanner) {
-        int n = scanner.nextInt();
-        int[] cards = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            cards[i] = scanner.nextInt();
-        }
-
-        int m = scanner.nextInt();
-        int[] targetCards = new int[m];
-
-        for (int i = 0; i < m; i++) {
-            targetCards[i] = scanner.nextInt();
-        }
-
-        ArrayList<int[]> question = new ArrayList<>();
-        question.add(cards);
-        question.add(targetCards);
-
-        return question;
-
     }
-
-}
